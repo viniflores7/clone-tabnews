66b25807173a2b4058f5b311e34aeebe3fe3be08
Vinícius Flores <160434409+viniflores7@users.noreply.github.com>
2024-04-09 20:21:32 +0000

    adiciona os scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto cópia do tabnews para estudo das tecnologias.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home (){
-    return <h1>Vida, eu te amo muito, se você me ama, dá uma risadinha :)</h1>
+function Home() {
+  return <h1>Vida, eu te amo muito, se você me ama, dá uma risadinha :)</h1>;
 }
 
 export default Home;
