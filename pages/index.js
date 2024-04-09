395812d19b1fcfc14feae52f08a7f821e3fdbe0d
Vinícius Flores <160434409+viniflores7@users.noreply.github.com>
2024-04-09 17:09:39 +0000

    voltando a origem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home (){
-    let nome = <h1>Vida, eu te amo muito, se você me ama, dá uma risadinha :)</h1>
-    let jonalcy = <p>E o cu em</p>
-    return nome, jonalcy
+    return <h1>Vida, eu te amo muito, se você me ama, dá uma risadinha :)</h1>
 }
 
 export default Home;
