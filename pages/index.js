04430f19e688b60cfe3df3deb0312d154458079b
Vinícius Flores <160434409+viniflores7@users.noreply.github.com>
2024-04-09 14:12:03 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Vida, eu te amo muito, se você me ama, dá uma risadinha :)</h1>
+    return <h1>Vida, eu te amo muito, se você me ama, dá uma risadinha :)</h1>, <p>E o cu em</p>
 }
 
 export default Home;
